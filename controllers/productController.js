86fc7728056631aef61596b7094a38315aec75ac
tndev <tndevfactory@gmail.com>
2024-09-04 19:13:39 +0100

    productController added console.log

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,6 +12,7 @@ const getProducts = asyncHandler(async (req, res) => {
 });
 
 const createProduct = asyncHandler(async (req, res) => {
+  console.log("changement branch chawki");
   const { designation, prix, couleur, taille, description } = req.body;
   const product = await Product.create(req.body);
   res.status(200).json({ success: true, product });
